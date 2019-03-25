0b03533ca15298babb7973939f9a9966bc47a72e
Clayton Wong <clayton.wong@ookla.com>
2019-03-25 11:39:03 -0700

    create new cask 'testing'

--- test.rb
+++ test.rb
@@ -1,11 +1,11 @@
-cask 'test' do
+cask 'testing' do
     version '1.0.0'
-    sha256 '7204752c8cd1e5f08748ba3d0ad6af840581161a645315144b78dbf2a945d10a'
+    sha256 '0cd1857999ed9f33a0041187dfde82cfa7b9995f1661128ef0e53255c3ddfdff'
     # github.com/claytonjwong/cli was verified as official when first introduced to the cask
-    url "https://github.com/claytonjwong/cask/raw/master/ookla-speedtest-1.0.0.35-devel.9aecb85-macosx.tgz"
-    name 'Speedtest'
-    homepage 'https://www.speedtest.net'
+    url "https://github.com/claytonjwong/cask/raw/master/testing.tgz"
+    name 'testing'
+    homepage 'https://www.testing.com'
 
-    binary 'speedtest'
+    binary 'testing'
 
 end
