c51668425bdec65f4c409127e5161e4cf1d0e67a
Clayton Wong <clayton.wong@ookla.com>
2019-04-06 12:51:28 -0700

    latest binary to test with

--- spoon.rb
+++ spoon.rb
@@ -1,11 +1,11 @@
-cask 'spoon.rb' do
+cask 'spoon' do
   version '0.0.1'
-  sha256 '6401298ebbc6edd14c89fb1378f3984584e75d70fdade68b2cf51dc44b8df818'
+  sha256 '01b40cd58f1b97c52155cfc39e30a723fd472f21191e6dd0d6a128f59863230c'
 
-  # /github.com/claytonjwong/cask was verified as official when first introduced to the cask
-  url "https://github.com/claytonjwong/cask/raw/master/testing.tgz"
+  # github.com/claytonjwong/cask was verified as official when first introduced to the cask
+  url 'https://github.com/claytonjwong/cli/raw/master/ookla-speedtest-1.0.0.53-devel.4ad6b7a-macosx.tgz'
   name 'spoon'
-  homepage 'https://www.spoon.com'
+  homepage 'https://www.spoon.com/'
 
   binary 'spoon'
 end
